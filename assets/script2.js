b34b9f8c23fabeed2553601990ac48a96dbf3223
Guillermo Steenbecker <g.steenbecker@gmail.com>
2024-06-16 22:20:05 -0400

    punto 2.1, 2.2 y 2.3

---
+++ assets/script2.js
@@ -0,0 +1,6 @@
+// function pintar(event) {
+//     event.target.style.backgroundColor = 'yellow';
+// }
+
+// const ele = document.getElementById("ele1");
+// ele.addEventListener("click", pintar);
